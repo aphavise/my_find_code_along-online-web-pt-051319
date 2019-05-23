b2d94b33a757a1d3c9c353dde006eb88da814b27
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-05-23 03:51:28 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,6 +3,9 @@ require 'pry'
 def my_find(collection)
    i = 0 
    x = [ ]
-   while i < collection.length 
+    while i < collection.length 
       x << yield(collection [ ])
+      i +=1 
+    end
+    x
 end
